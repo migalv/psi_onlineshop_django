378db2e111d2ea58673023fac4e1aec8f0fc95d2
Your Name <you@example.com>
2017-11-10 17:34:11 +0100

    Empezando a hacer el views.py

--- shop/views.py
+++ shop/views.py
@@ -6,26 +6,43 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from shop.models import Category, Product
 
+	
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+	# Construct a dictionary to pass to the template engine as its context.
+	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
+	context_dict = {'boldmessage': "I am bold font from the context"}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+	# Return a rendered response to send to the client.
+	# We make use of the shortcut function to make our lives easier.
+	# Note that the first parameter is the template we wish to use.
 
-    return render(request, 'shop/index.html', context_dict)
+	return render(request, 'shop/index.html', context_dict)
 
 def base(request):
 	return render(request, 'shop/base.html')
 
 def product_list(request, catSlug=None):
+
+	try:
+		categories = Category.objects.all()
+		products = Product.objects.all()
+		category = None
+	except Category.DoesNotExists:
+		category = Category.objects.get(catSlug=catSlug)
+		try:
+			products = Product.objects.filter(category=category)
+		except Category.DoesNotExists:
 	
-	#Your code goes here
-	#queries that fill, category, categories and products
 	return render(request,'shop/list.html',
-				{'category': category,
-				'categories': categories,
-				'products': products})
+					{'category': category,
+					'categories': categories,
+					'products': products})
+
+def detailProduct(request, prodId, prodSlug):
+	
+	product = {'category': category.catName, }
+
+	#Your code goes here
+	#query that returns a product with id=protId	
+	return render(request, 'shop/detail.html', {'product': product})
